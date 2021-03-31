911b04a43f0e1715a1a144bc4e9cc99109f59078
Androz2091 <androz2091@gmail.com>
2021-03-31 18:56:09 +0200

    :bug: Finish store

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { StoreProvider } from 'easy-peasy';
 import Home from './pages/Home';
 import NavigationBar from './components/NavigationBar';
+import store from './store';
 
 function App() {
     return (
-        <Router>
-            <NavigationBar />
-            <Switch>
-                <Route path="/" component={Home} />
-            </Switch>
-        </Router>
+        <StoreProvider store={store}>
+            <Router>
+                <NavigationBar />
+                <Switch>
+                    <Route path="/" component={Home} />
+                </Switch>
+            </Router>
+        </StoreProvider>
     );
 }
 
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,34 +1,45 @@
+import { useStoreState } from 'easy-peasy';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavigationBar.css';
 
-const NavigationBar = () => (
-    <nav>
-        <input id="nav-toggle" type="checkbox" />
-        <div className="logo">
-            <img className="img" src={`${process.env.PUBLIC_URL}/assets/logo.png`} alt="Logo" height="40px" />
-            <Link to="/">ManageInvite</Link>
-        </div>
-        <ul className="links">
-            <li><Link to="/status">Status</Link></li>
-            <li><Link to="/docs">Documentation</Link></li>
-            <li><Link to="/support">Support server</Link></li>
-            <li>
-                <Link
-                    to="/login"
-                    className="login-button"
-                >
-                    Dashboard
-                </Link>
+const NavigationBar = () => {
+    const currentUser = useStoreState((state) => state.currentUser);
 
-            </li>
-        </ul>
-        <label className="icon-burger" htmlFor="nav-toggle">
-            <div className="line" />
-            <div className="line" />
-            <div className="line" />
-        </label>
-    </nav>
-);
+    const login = () => {
+
+    };
+
+    return (
+        <nav>
+            <input id="nav-toggle" type="checkbox" />
+            <div className="logo">
+                <img className="img" src={`${process.env.PUBLIC_URL}/assets/logo.png`} alt="Logo" height="40px" />
+                <Link to="/">ManageInvite</Link>
+            </div>
+            <ul className="links">
+                <li><Link to="/status">Status</Link></li>
+                <li><Link to="/docs">Documentation</Link></li>
+                <li><Link to="/support">Support server</Link></li>
+                <li>
+                    {!currentUser ? (
+                        <button
+                            type="button"
+                            onClick={login}
+                            className="login-button"
+                        >
+                            Dashboard
+                        </button>
+                    ) : <h1>Logged</h1> }
+                </li>
+            </ul>
+            <label className="icon-burger" htmlFor="nav-toggle">
+                <div className="line" />
+                <div className="line" />
+                <div className="line" />
+            </label>
+        </nav>
+    );
+};
 
 export default NavigationBar;
