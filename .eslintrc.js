854f4ef7e182cffa724b4714a1aa735aeedf3692
Androz2091 <androz2091@gmail.com>
2021-03-31 11:23:37 +0200

    :art: Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb'
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    plugins: [
+        'react'
+    ],
+    rules: {
+        indent: ['error', 4],
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'comma-dangle': ['error', 'never']
+    }
+};
