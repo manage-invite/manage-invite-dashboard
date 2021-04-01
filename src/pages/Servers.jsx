3f34050c9acb3f7ea434dfa08f5f07f49de1e681
Androz2091 <androz2091@gmail.com>
2021-04-01 21:19:09 +0200

    :sparkles: Finish servers page

--- src/pages/Servers.jsx
+++ src/pages/Servers.jsx
@@ -1,26 +1,41 @@
+import { useStoreState } from 'easy-peasy';
 import React from 'react';
 import FakeServer from '../components/FakeServer';
 import Server from '../components/Server';
 
-const Servers = () => (
-    <div>
-        <h1 style={{
-            fontFamily: 'Poppins',
-            textTransform: 'uppercase',
-            textAlign: 'center'
-        }}
-        >
-            Select a server
-        </h1>
-        <div style={{
-            maxWidth: '700px',
-            margin: '0 auto'
-        }}
-        >
-            <Server />
-            <FakeServer />
+const Servers = () => {
+    const userGuildsCache = useStoreState((state) => state.userGuildsCache);
+
+    return (
+        <div>
+            <h1 style={{
+                fontFamily: 'Poppins',
+                textTransform: 'uppercase',
+                textAlign: 'center'
+            }}
+            >
+                Select a server
+            </h1>
+            <div style={{
+                maxWidth: '700px',
+                margin: '0 auto'
+            }}
+            >
+                {userGuildsCache ? (
+                    userGuildsCache.map((guild) => (
+                        <Server serverName={guild.name} serverIconURL={guild.iconURL} serverStatus="premium" />
+                    ))
+                ) : (
+                    <>
+                        <FakeServer />
+                        <FakeServer />
+                        <FakeServer />
+                        <FakeServer />
+                    </>
+                )}
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Servers;
