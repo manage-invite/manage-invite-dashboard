7ea293c499a020feb38a8671be9ff8d213230345
Androz2091 <androz2091@gmail.com>
2021-04-08 17:13:41 +0200

    :sparkles: Clean up othr files

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NotFound = () => (
+    <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 'calc(70vh)'
+    }}
+    >
+        We can&apos;t find the page you are looking for... 😕
+    </div>
+);
+
+export default NotFound;
--- src/pages/ServerSettings.jsx
+++ src/pages/ServerSettings.jsx
@@ -1,39 +1,15 @@
 import { useStoreState } from 'easy-peasy';
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import LoadingAnimation from '../components/LoadingAnimation';
 import Settings from '../components/Settings';
+import NotFound from './NotFound';
 
 const ServerSettings = () => {
     const userGuildsCache = useStoreState((state) => state.userGuildsCache);
     const { id } = useParams();
     const cachedGuild = userGuildsCache?.find((guild) => guild.id === id);
 
-    const loadingAnimation = (
-        <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: 'calc(70vh)'
-        }}
-        >
-            <LoadingAnimation size="20px" />
-        </div>
-    );
-
-    /*
-    useEffect(() => {
-        setTimeout(() => {
-            if (!cachedGuild) unknownGuild = true;
-        }, 1000);
-    }, []);
-    */
-
-    return (
-        !cachedGuild ? loadingAnimation : (
-            <Settings serverName={cachedGuild.name} />
-        )
-    );
+    return cachedGuild ? <Settings serverName={cachedGuild.name} /> : <NotFound />;
 };
 
 export default ServerSettings;
