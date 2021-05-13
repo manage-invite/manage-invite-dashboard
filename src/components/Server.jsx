1e8e803a699e400a6de65c19dd3e8bafe6c0ef77
Androz2091 <androz2091@gmail.com>
2021-05-13 16:13:39 +0200

    :sparkles: Add premium pop-up

--- src/components/Server.jsx
+++ src/components/Server.jsx
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import './Server.css';
 import { useStoreActions, useStoreState } from 'easy-peasy';
 import { useHistory } from 'react-router-dom';
+import swal from '@sweetalert/with-react';
 import { socket, ensureSocketConnected } from '../socket';
 
 const Server = ({
@@ -56,6 +57,184 @@ const Server = ({
             });
         } else if (manageButton) {
             history.push(`/servers/${serverID}`);
+        } else {
+            swal({
+                buttons: {
+                    cancel: 'Cancel',
+                    why: {
+                        text: 'Ask for a 7-day trial',
+                        className: 'swal-button-primary'
+                    },
+                    confirm: {
+                        text: 'Buy',
+                        className: 'swal-button-confirm'
+                    }
+                },
+                content: (
+                    <div style={{
+                        textAlign: 'start'
+                    }}
+                    >
+                        <h3 style={{
+                            textAlign: 'center'
+                        }}
+                        >
+                            ManageInvite Premium
+
+                        </h3>
+                        { /* eslint-disable-next-line max-len */ }
+                        <p>ManageInvite Premium is required to use the bot. This is the only way we can guarantee the best possible experience for all our users!</p>
+                        <div style={{
+                            textAlign: 'start',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignContent: 'flex-start'
+                        }}
+                        >
+                            <div style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                marginTop: '0.3rem'
+                            }}
+                            >
+                                <svg
+                                    style={{
+                                        width: '2rem',
+                                        marginRight: '0.3rem'
+                                    }}
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                                <h4 style={{
+                                    margin: 0,
+                                    marginLeft: '0.3rem'
+                                }}
+                                >
+                                    Fast responses
+                                </h4>
+                            </div>
+                            <div style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                marginTop: '0.3rem'
+                            }}
+                            >
+                                <svg
+                                    style={{
+                                        width: '2rem',
+                                        marginRight: '0.3rem'
+                                    }}
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+                                </svg>
+                                <h4 style={{
+                                    margin: 0,
+                                    marginLeft: '0.3rem'
+                                }}
+                                >
+                                    Intensively customizable
+                                </h4>
+                            </div>
+                            <div style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                marginTop: '0.3rem'
+                            }}
+                            >
+                                <svg
+                                    style={{
+                                        width: '2rem',
+                                        marginRight: '0.3rem'
+                                    }}
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+
+                                </svg>
+                                <h4 style={{
+                                    margin: 0,
+                                    marginLeft: '0.3rem'
+                                }}
+                                >
+                                    Very reliable for counting invitations
+                                </h4>
+                            </div>
+                            <div style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                marginTop: '0.3rem'
+                            }}
+                            >
+                                <svg
+                                    style={{
+                                        width: '2rem',
+                                        marginRight: '0.3rem'
+                                    }}
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+
+                                </svg>
+                                <h4 style={{
+                                    margin: 0,
+                                    marginLeft: '0.3rem'
+                                }}
+                                >
+                                    Awesome web dashboard
+                                </h4>
+                            </div>
+                            <div style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                marginTop: '0.3rem'
+                            }}
+                            >
+                                <svg
+                                    style={{
+                                        width: '2rem',
+                                        marginRight: '0.3rem'
+                                    }}
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
+
+                                </svg>
+                                <h4 style={{
+                                    margin: 0,
+                                    marginLeft: '0.3rem'
+                                }}
+                                >
+                                    Advanced features
+                                </h4>
+                            </div>
+                            <p style={{
+                                marginBottom: 0
+                            }}
+                            >
+                                Premium is available for the symbolic price of $2/month.
+
+                            </p>
+                        </div>
+                    </div>
+                )
+            });
         }
     };
 
