c81760c4252c8d2d98d12eb1df2d424753e66730
Androz2091 <androz2091@gmail.com>
2021-05-14 10:46:24 +0200

    :sparkles: Update navbar

--- src/components/layout/NavigationBar.jsx
+++ src/components/layout/NavigationBar.jsx
@@ -102,8 +102,8 @@ const NavigationBar = () => {
             </div>
             <ul className="links">
                 <li><Link to="/status">Status</Link></li>
-                <li><Link to="/docs">Documentation</Link></li>
-                <li><Link to="/support">Support server</Link></li>
+                <li><a href="https://docs.manage-invite.xyz" target="_blank" rel="noreferrer">Documentation</a></li>
+                <li><a href={process.env.REACT_APP_DISCORD_URL} target="_blank" rel="noreferrer">Support server</a></li>
                 <li>
                     {dashboardButton}
                 </li>
